beb75230a0a5f599e36d64d5bbe19fe2e425a185
dmoralesdev <9662549+dmoralesdev@users.noreply.github.com>
2018-05-15 21:40:37 -0600

    Version 2 - Addition of Network Node type and some cleanup

--- README.md
+++ README.md
@@ -4,21 +4,36 @@ This is the Presence-Poly for the [Universal Devices ISY994i](https://www.univer
 (c) 2018 Diego Morales
 MIT license.
 
-This node server is intended to use the onboard Bluetooth support that comes in a Raspberry Pi 3 to monitor for the "presence" of other bluetooth devices. This can help determine indoor positioning with respect to your RPi. In theory, this should also work with RPi 1-2 with an external bluetooth adapter, but it has not been tested under those conditions (please let me me know if it works).
+There are 2 node types on Presence-Poly: Presence and Network
+
+##Bluetooth Node:
+This node type is intended to use the onboard Bluetooth support that comes in a Raspberry Pi 3 to monitor for the "presence" of other bluetooth devices. This can help determine indoor positioning with respect to your RPi. In theory, this should also work with RPi 1-2 with an external bluetooth adapter, but it has not been tested under those conditions (please let me me know if it works).
 
 Each node will have 3 values: In Range, Proximity, and Scanning
 
 1. In Range: True or False for easiness in determining if the monitored device is detected or not by the RPi
-2. Proximity: Value from 0 (not In Range) to 5 (Full signal)
+2. Proximity: Value from 0 (Out of range) to 5 (Full signal)
 3. Scanning: True or False to allow the node to "sleep" in case you want to temporarily disable a node (the value gets reset after a restart)
 
-### UPDATE v. 1.5! Now proximity is based on RSSI. You can check the original value in the NodeServer logs, but this is basically getting translated to a more usable value. The ranges go like this:
+Proximity is based on RSSI. You can check the original value in the NodeServer logs, but this is basically getting translated to a more usable value. The ranges go like this:
 
 1. Proximity 5 -> RSSI 0 (highest value which means the device is as close as it can be to the RPi)
 2. Proximity 4 -> RSSI -1 to RSSI -5
 3. Proximity 3 -> RSSI -6 to RSSI -15
 4. Proximity 2 -> RSSI -16 to RSSI -35
 5. Proximity 1 -> RSSI -36 and beyond
+6. Proximity 0 -> Out of range
+
+##Network Node:
+This node type is intended to monitor for the "presence" of a device inside of the same network the RPi is connected to. It does this by sending 1 ICMP Ping package to the IP of the monitored device.
+
+Each node will have 3 values: On Network, Strength, and Scanning
+
+1. On Network: True or False for easiness in determining if the monitored device is detected or not by the RPi
+2. Strength: Value from 0 (Out of network) to 5 (No Faults)
+3. Scanning: True or False to allow the node to "sleep" in case you want to temporarily disable a node (the value gets reset after a restart)
+
+Strength is based on the number of consecutive successful pings. It will decrease with each dropped ping package until it reaches 0, which means the device is off the network. It could also be seen as an "inverted fault counter".
 
 
 One of the coolest features of Presence-Poly is that it can be installed into as many RPis (w/Polyglot) as needed in order to create a "network" of monitoring devices to allow evaluations via ISY programs such as "At Home", "Left Home", "In Kitchen", "In Bedroom". Your imagination is the limit! Please see this link for more info on this: https://forum.universal-devices.com/topic/24146-can-2-different-rpis-running-polyglot-connect-to-the-same-isy/
@@ -31,8 +46,14 @@ One of the coolest features of Presence-Poly is that it can be installed into as
 3. Click Add NodeServer in the NodeServer menu of Polyglot, and choose an available slot
 4. Go to the NodeServer details from the dashboard, and then click on Configuration
 5. Add as many devices you need to monitor in the form
-    - Key: Name of the device (accepts spaces)
-    - Value: Bluetooth ID of your device (in the form of DF:34:45:D3:B1:E9)
+    Bluetooth:
+        - Key: Name of the device (accepts spaces)
+        - Value: Bluetooth ID of your device (in the form of DF:34:45:D3:B1:E9)
+    Network:
+        - Key: Name of the device (accepts spaces)
+        - Value: IP address of your device (in the form of 192.168.0.1)
+        
+    *No 2 keys can be the same, so if you are monitoring a device both on bluetooth and network, the key (name) would need to be different for each type. For example, you could append the type to the name like: "iPhone-Bluetooth", "iPhone-Network"
 6. Restart the NodeServer
 7. Close/Open your Admin Console and you should now see a "Presence Controller" node, with all the node children you created
 
--- presence-poly.py
+++ presence-poly.py
@@ -7,100 +7,46 @@ import bluetooth._bluetooth as bt
 import struct
 import array
 import fcntl
+import os
 
 LOGGER = polyinterface.LOGGER
-"""
-polyinterface has a LOGGER that is created by default and logs to:
-logs/debug.log
-You can use LOGGER.info, LOGGER.warning, LOGGER.debug, LOGGER.error levels as needed.
-"""
 
 class PresenceController(polyinterface.Controller):
-    """
-    The Controller Class is the primary node from an ISY perspective. It is a Superclass
-    of polyinterface.Node so all methods from polyinterface.Node are available to this
-    class as well.
 
-    Class Variables:
-    self.nodes: Dictionary of nodes. Includes the Controller node. Keys are the node addresses
-    self.name: String name of the node
-    self.address: String Address of Node, must be less than 14 characters (ISY limitation)
-    self.polyConfig: Full JSON config dictionary received from Polyglot for the controller Node
-    self.added: Boolean Confirmed added to ISY as primary node
-    self.config: Dictionary, this node's Config
-
-    Class Methods (not including the Node methods):
-    start(): Once the NodeServer config is received from Polyglot this method is automatically called.
-    addNode(polyinterface.Node, update = False): Adds Node to self.nodes and polyglot/ISY. This is called
-        for you on the controller itself. Update = True overwrites the existing Node data.
-    updateNode(polyinterface.Node): Overwrites the existing node data here and on Polyglot.
-    delNode(address): Deletes a Node from the self.nodes/polyglot and ISY. Address is the Node's Address
-    longPoll(): Runs every longPoll seconds (set initially in the server.json or default 10 seconds)
-    shortPoll(): Runs every shortPoll seconds (set initially in the server.json or default 30 seconds)
-    query(): Queries and reports ALL drivers for ALL nodes to the ISY.
-    getDriver('ST'): gets the current value from Polyglot for driver 'ST' returns a STRING, cast as needed
-    runForever(): Easy way to run forever without maxing your CPU or doing some silly 'time.sleep' nonsense
-                  this joins the underlying queue query thread and just waits for it to terminate
-                  which never happens.
-    """
     def __init__(self, polyglot):
-        """
-        Optional.
-        Super runs all the parent class necessities. You do NOT have
-        to override the __init__ method, but if you do, you MUST call super.
-        """
         super(PresenceController, self).__init__(polyglot)
         self.name = 'Presence Controller'
 
     def start(self):
-        """
-        Optional.
-        Polyglot v2 Interface startup done. Here is where you start your integration.
-        This will run, once the NodeServer connects to Polyglot and gets it's config.
-        In this example I am calling a discovery method. While this is optional,
-        this is where you should start. No need to Super this method, the parent
-        version does nothing.
-        """
         LOGGER.info('Presence Controller started.')
         self.check_params()
         self.discover()
 
     def shortPoll(self):
-        """
-        Optional.
-        This runs every 10 seconds. You would probably update your nodes either here
-        or longPoll. No need to Super this method the parent version does nothing.
-        The timer can be overriden in the server.json.
-        """
+        #This is where the updates to each node happen
         for node in self.nodes:
-            self.nodes[node].checkPresence()
+            self.nodes[node].update()
 
     def longPoll(self):
-        """
-        Optional.
-        This runs every 60 seconds. You would probably update your nodes either here
-        or shortPoll. No need to Super this method the parent version does nothing.
-        The timer can be overriden in the server.json.
-        """
+        #Not used
         pass
 
     def query(self):
-        """
-        Optional.
-        By default a query to the control node reports the FULL driver set for ALL
-        nodes back to ISY. If you override this method you will need to Super or
-        issue a reportDrivers() to each node manually.
-        """
         for node in self.nodes:
             self.nodes[node].reportDrivers()
 
     def discover(self, *args, **kwargs):
+        #Discover nodes and add them by type
         for key,val in self.polyConfig['customParams'].items():
             #LOGGER.debug(key + " => " + val)
-            blueid = val.replace(':','').lower()
-            self.addNode(PresenceNode(self, self.address, blueid, key))
+            if (val.find(':') != -1):
+                blueid = val.replace(':','').lower()
+                self.addNode(BluetoothNode(self, self.address, blueid, key))
+            elif (val.find('.') != -1):
+                netip = val.replace('.','')
+                self.addNode(NetworkNode(self, self.address, netip, val, key))
 
-    def checkPresence(self):
+    def update(self):
         pass    
 
     def delete(self):
@@ -123,14 +69,6 @@ class PresenceController(polyinterface.Controller):
         st = self.poly.installprofile()
         return st
 
-    """
-    Optional.
-    Since the controller is the parent node in ISY, it will actual show up as a node.
-    So it needs to know the drivers and what id it will use. The drivers are
-    the defaults in the parent Class, so you don't need them unless you want to add to
-    them. The ST variable is for reporting status through Polyglot to ISY,
-    DO NOT remove them. UOM 2 is boolean.
-    """
     id = 'presence_controller'
     commands = {
         'DISCOVER': discover,
@@ -141,9 +79,10 @@ class PresenceController(polyinterface.Controller):
         {'driver': 'ST', 'value': 0, 'uom': 2}
     ]
 
-class BluetoothRSSI(object):
+class BlueHelper(object):
 
     def __init__(self, addr):
+        #Initializes bluetooth object
         self.addr = addr
         self.hci_sock = bt.hci_open_dev()
         self.hci_fd = self.hci_sock.fileno()
@@ -152,89 +91,54 @@ class BluetoothRSSI(object):
         self.connected = False
         self.cmd_pkt = None
 
-    def prep_cmd_pkt(self):
-        """Prepares the command packet for requesting RSSI"""
+    def prepare_command(self):
+        #Creates the command
         reqstr = struct.pack(
             "6sB17s", bt.str2ba(self.addr), bt.ACL_LINK, bytes("\0",'utf-8') * 17)
         request = array.array("b", reqstr)
         handle = fcntl.ioctl(self.hci_fd, bt.HCIGETCONNINFO, request, 1)
-        handle = struct.unpack("8xH14x", request.tostring())[0]
+        handle = struct.unpack("8xH14x", request.tobytes())[0]
         self.cmd_pkt = struct.pack('H', handle)
 
     def connect(self):
-        """Connects to the Bluetooth address"""
-        self.bt_sock.connect_ex((self.addr, 1))  # PSM 1 - Service Discovery
+        #Connects to the bluetooth device
+        self.bt_sock.connect_ex((self.addr, 1))
         self.connected = True
 
     def get_rssi(self):
-        """Gets the current RSSI value.
-        @return: The RSSI value (float) or None if the device connection fails
-                 (i.e. the device is nowhere nearby).
-        """
+        #Gets the RSSI value
         try:
             # Only do connection if not already connected
             if not self.connected:
                 self.connect()
             if self.cmd_pkt is None:
-                self.prep_cmd_pkt()
+                self.prepare_command()
             # Send command to request RSSI
             rssi = bt.hci_send_req(
                 self.hci_sock, bt.OGF_STATUS_PARAM,
                 bt.OCF_READ_RSSI, bt.EVT_CMD_COMPLETE, 4, self.cmd_pkt)
             rssi = struct.unpack('b', rssi[3:4])[0]
             return rssi
-        except IOError:
-            # Happens if connection fails (e.g. device is not in range)
+        except IOError as ioerr:
+            # Happens if connection fails
+            #LOGGER.debug("I/O error: {0}".format(ioerr))
             self.connected = False
             return None
 
-class PresenceNode(polyinterface.Node):
-    """
-    This is the class that all the Nodes will be represented by. You will add this to
-    Polyglot/ISY with the controller.addNode method.
-
-    Class Variables:
-    self.primary: String address of the Controller node.
-    self.parent: Easy access to the Controller Class from the node itself.
-    self.address: String address of this Node 14 character limit. (ISY limitation)
-    self.added: Boolean Confirmed added to ISY
-
-    Class Methods:
-    start(): This method is called once polyglot confirms the node is added to ISY.
-    setDriver('ST', 1, report = True, force = False):
-        This sets the driver 'ST' to 1. If report is False we do not report it to
-        Polyglot/ISY. If force is True, we send a report even if the value hasn't changed.
-    reportDrivers(): Forces a full update of all drivers to Polyglot/ISY.
-    query(): Called when ISY sends a query request to Polyglot for this specific node
-    """
+class BluetoothNode(polyinterface.Node):
     def __init__(self, controller, primary, address, name):
-        """
-        Optional.
-        Super runs all the parent class necessities. You do NOT have
-        to override the __init__ method, but if you do, you MUST call super.
-
-        :param controller: Reference to the Controller class
-        :param primary: Controller address
-        :param address: This nodes address
-        :param name: This nodes name
-        """
-        super(PresenceNode, self).__init__(controller, primary, address, name)
+        super(BluetoothNode, self).__init__(controller, primary, address, name)
+        self.blueid = ':'.join(self.address[i:i+2] for i in range(0, len(self.address), 2)).upper()
 
     def start(self):
-        """
-        Optional.
-        This method is run once the Node is successfully added to the ISY
-        and we get a return result from Polyglot. Only happens once.
-        """
         self.setOn('DON')
         
-    def checkPresence(self):
+    def update(self):
         if (self.scan):
-            blueid = ':'.join(self.address[i:i+2] for i in range(0, len(self.address), 2)).upper()
-            btrssi = BluetoothRSSI(addr=blueid)
-            result = btrssi.get_rssi()
+            btnode = BlueHelper(addr=self.blueid)
+            result = btnode.get_rssi()
             if (result != None):
-                LOGGER.debug(blueid + ': In range. RSSI: ' + str(result))
+                LOGGER.debug('Bluetooth ' + self.blueid + ': In range. RSSI: ' + str(result))
                 if (result >=0):
                     self.setInRange(5)
                 elif (result < 0 and result >= -5):
@@ -247,9 +151,9 @@ class PresenceNode(polyinterface.Node):
                     self.setInRange(1)
             elif (self.proximity > 1):
                 self.setInRange(self.proximity - 1)
-                LOGGER.debug(blueid + ': In Fault')
+                LOGGER.debug('Bluetooth ' + self.blueid + ': In Fault')
             elif (self.proximity == 1):
-                LOGGER.debug(blueid + ': Out of range')
+                LOGGER.debug('Bluetooth ' + self.blueid + ': Out of range')
                 self.setOutRange()
             
     def setInRange(self,prox):
@@ -263,31 +167,16 @@ class PresenceNode(polyinterface.Node):
         self.setDriver('GV0', self.proximity)
     
     def setOn(self, command):
-        """
-        Example command received from ISY.
-        Set DON on MyNode.
-        Sets the ST (status) driver to 1 or 'True'
-        """
         self.setOutRange()
         self.setDriver('GV1', 1)
         self.scan = 1
 
     def setOff(self, command):
-        """
-        Example command received from ISY.
-        Set DOF on MyNode
-        Sets the ST (status) driver to 0 or 'False'
-        """
         self.setOutRange()
         self.setDriver('GV1', 0)
         self.scan = 0
 
     def query(self):
-        """
-        Called by ISY to report all drivers for this node. This is done in
-        the parent class, so you don't need to override this method unless
-        there is a need.
-        """
         self.reportDrivers()
 
 
@@ -296,46 +185,94 @@ class PresenceNode(polyinterface.Node):
         {'driver': 'GV0', 'value': 0, 'uom': 56},
         {'driver': 'GV1', 'value': 1, 'uom': 2}
     ]
-    """
-    Optional.
-    This is an array of dictionary items containing the variable names(drivers)
-    values and uoms(units of measure) from ISY. This is how ISY knows what kind
-    of variable to display. Check the UOM's in the WSDK for a complete list.
-    UOM 2 is boolean so the ISY will display 'True/False'
-    """
-    id = 'presence_node'
-    """
-    id of the node from the nodedefs.xml that is in the profile.zip. This tells
-    the ISY what fields and commands this node has.
-    """
+
+    id = 'bluetooth_node'
+
     commands = {
                     'DON': setOn, 'DOF': setOff
                 }
-    """
-    This is a dictionary of commands. If ISY sends a command to the NodeServer,
-    this tells it which method to call. DON calls setOn, etc.
-    """
+
+class PingHelper(object):
+
+    def __init__(self, ip, timeout):
+        self.ip = ip
+        self.timeout = timeout
+
+    def ping(self):
+        try:
+            response = os.system("ping -c 1 -W " + str(self.timeout-1) + " " + self.ip)
+            if response == 0:
+                return response
+            else:
+                return None
+        except:
+            # Capture any exception
+            return None
+                
+class NetworkNode(polyinterface.Node):
+
+    def __init__(self, controller, primary, address, ipaddress, name):
+        super(NetworkNode, self).__init__(controller, primary, address, name)
+        self.ip = ipaddress
+        
+    def start(self):
+        self.setOn('DON')
+        
+    def update(self):
+        if (self.scan):
+            onnet = PingHelper(ip=self.ip,timeout=self.parent.polyConfig['shortPoll'])
+            result = onnet.ping()
+            if (result != None):
+                LOGGER.debug('Network ' + self.ip + ': On Network')
+                self.setOnNetwork(5)
+            elif (self.strength > 1):
+                self.setOnNetwork(self.strength - 1)
+                LOGGER.debug('Network ' + self.ip + ': In Fault')
+            elif (self.strength == 1):
+                LOGGER.debug('Network ' + self.ip + ': Out of Network')
+                self.setOffNetwork()
+            
+    def setOnNetwork(self,strength):
+        self.setDriver('ST', 1)
+        self.strength = strength
+        self.setDriver('GV0', self.strength)
+        
+    def setOffNetwork(self):
+        self.setDriver('ST', 0)
+        self.strength = 0
+        self.setDriver('GV0', self.strength)
+    
+    def setOn(self, command):
+        self.setOffNetwork()
+        self.setDriver('GV1', 1)
+        self.scan = 1
+
+    def setOff(self, command):
+        self.setOffNetwork()
+        self.setDriver('GV1', 0)
+        self.scan = 0
+
+    def query(self):
+        self.reportDrivers()
+
+
+    drivers = [
+        {'driver': 'ST', 'value': 0, 'uom': 2},
+        {'driver': 'GV0', 'value': 0, 'uom': 56},
+        {'driver': 'GV1', 'value': 1, 'uom': 2}
+    ]
+
+    id = 'network_node'
+
+    commands = {
+                    'DON': setOn, 'DOF': setOff
+                }
 
 if __name__ == "__main__":
     try:
         polyglot = polyinterface.Interface('PresenceController')
-        """
-        Instantiates the Interface to Polyglot.
-        """
         polyglot.start()
-        """
-        Starts MQTT and connects to Polyglot.
-        """
         control = PresenceController(polyglot)
-        """
-        Creates the Controller Node and passes in the Interface
-        """
         control.runForever()
-        """
-        Sits around and does nothing forever, keeping your program running.
-        """
     except (KeyboardInterrupt, SystemExit):
         sys.exit(0)
-        """
-        Catch SIGTERM or Control-C and exit cleanly.
-        """
